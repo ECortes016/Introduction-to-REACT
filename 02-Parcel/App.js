408051de4a7cd0600d9cb52a49c3f8f46068dec2
Emmanuel Cortes <ecortes016@west-mec.org>
2019-11-20 18:25:15 -0700

    React App
    Created a react app with components

---
+++ 02-Parcel/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Jumbotron from './components/Jumbotron';
+
+
+// export default function app() {
+//     return (
+//         <div>
+//             My first apppppppppp!
+//         </div>
+//     )
+// }
+
+export default () => (<Jumbotron/>)
+
